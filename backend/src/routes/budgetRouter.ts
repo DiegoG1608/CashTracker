41d65b20c54d06bc4ed4c2bb97a38107f0474c4b
Diego González <diegoglez16082001@hotmail.com>
2026-05-13 13:00:37 -0600

    Routes for Expenses

--- backend/src/routes/budgetRouter.ts
+++ backend/src/routes/budgetRouter.ts
@@ -3,6 +3,7 @@ import { body, param } from 'express-validator'
 import { BudgetController } from "../controllers/BudgetController"
 import { handleInputErrors } from "../middleware/validation"
 import { validateBudgetExists, validateBudgetID, validateBudgetInput } from "../middleware/budget"
+import { ExpensesController } from "../controllers/ExpenseController"
 
 const router = Router()
 
@@ -28,6 +29,20 @@ router.put('/:budgetId',
 router.delete('/:budgetId', 
     BudgetController.deleteBudgetByID)
 
+//----------Routes for Expenses----------
+router.get('/:budgetId/expenses', 
+    ExpensesController.getAll)//Obtener todos los gastos de un presupuesto
 
+router.post('/:budgetId/expenses',
+    ExpensesController.create)//Crear un nuevo gasto para un presupuesto
+
+router.get('/:budgetId/expenses/:expenseId',
+    ExpensesController.getById)//
+
+router.put('/:budgetId/expenses/:expenseId',
+    ExpensesController.updateById)//Actualizar un gasto específico de un presupuesto
+
+router.delete('/:budgetId/expenses/:expenseId',
+    ExpensesController.deleteById)//
 
 export default router
